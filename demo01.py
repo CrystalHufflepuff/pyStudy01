607f2d809848cd32caf57d0998df386d50b0725e
Crystal <xiaomengjiang_hit@163.com>
2020-06-14 09:13:54 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,16 @@
+#print("hello word!")
+'''a=int ("23333")
+print(type(a))
+
+b=float(234.2)
+c=float(20.1)
+print(b+c)
+a="leishihaidaoyuan"
+print (len(a))'''
+a=input("请输入a：")
+b=input("请输入b:")
+la=len(a)
+lb=len(b)
+print("a的长度为：",la,"b的长度为：",lb)
+#print()
+print("a与b长度之和为：",int(la+lb))
