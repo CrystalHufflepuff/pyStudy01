f4aa767f96d9b2411e71fbc8c342339ba0acf03b
Crystal <xiaomengjiang_hit@163.com>
2020-06-14 09:19:07 +0800

    尝试向GitHub提交

--- demo01.py
+++ demo01.py
@@ -7,9 +7,11 @@ c=float(20.1)
 print(b+c)
 a="leishihaidaoyuan"
 print (len(a))'''
+
 a=input("请输入a：")
 b=input("请输入b:")
 la=len(a)
 lb=len(b)
 print("a的长度为：",la,"b的长度为：",lb)
 print("a与b长度之和为：",int(la+lb))
+print("exit")
